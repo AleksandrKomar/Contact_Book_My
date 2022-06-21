5f053ec0837c9afce5630aadaa0f8a93d45b8686
AK <nightmare8707@gmail.com>
2022-06-21 11:12:12 +0300

    pravka

--- src/myTask/Contact.java
+++ src/myTask/Contact.java
@@ -8,12 +8,19 @@ public class Contact implements ContactInfo {
     private String contactName;
 
     private int eMailNumber;
+    private int telCount;
     private Email[] mymail;
+
+    private Tel[] myTel;
 
     public Contact(String contactName) {
         this.contactName = contactName;
         this.eMailNumber = 0;
         this.mymail = new Email[3];
+
+        this.telCount = 0;
+        this.myTel = new Tel[1];
+
     }
 
     public void rename(String newName) {
@@ -59,8 +66,28 @@ public class Contact implements ContactInfo {
         return null;
     }
 
+
+    public void setTelCount() {
+        this.telCount++;
+    }
+
+    public int getTelCount() {
+        return telCount;
+    }
+
     public ContactInfo addPhoneNumber(int code, String number) {
-        return null;
+
+        if (getTelCount() >= 1) {
+            return null;
+        }
+
+        Tel t = new Tel(code, number);
+        myTel[getTelCount()] = t;
+
+        setTelCount();
+        return t;
+
+
     }
 
     public Social addTwitter(String twitterId) {
@@ -79,9 +106,27 @@ public class Contact implements ContactInfo {
     public ContactInfo[] getInfo() {
         //Implement this method
 
-        ArrayList<ContactInfo> list = new ArrayList<>();
+        int count = 0;
 
-        list.add(this);
+        int sizeArray = 1;
+
+        for (int i = 0; i < mymail.length; i++) {
+            if (mymail[i] == null) {
+                continue;
+            }
+            sizeArray++;
+        }
+
+        for (int i = 0; i < myTel.length; i++) {
+            if (myTel[i] == null) {
+                continue;
+            }
+            sizeArray++;
+        }
+
+        ContactInfo[] infos = new ContactInfo[sizeArray];
+
+        infos[count] = this;
 
         for (int i = 0; i < mymail.length; i++) {
 
@@ -89,14 +134,23 @@ public class Contact implements ContactInfo {
                 continue;
             }
 
-            list.add(i + 1, mymail[i]);
+            count++;
+            infos[count] = mymail[i];
+
         }
 
+        for (int i = 0; i < myTel.length; i++) {
 
-        ContactInfo[] array = new ContactInfo[list.size()];
-        list.toArray(array);
+            if (myTel[i] == null) {
+                continue;
+            }
 
-        return array;
+            count++;
+            infos[count] = myTel[i];
+
+        }
+
+        return infos;
     }
 
     @Override
@@ -138,8 +192,15 @@ public class Contact implements ContactInfo {
 
         private String email;
 
+        private String title;
+
         public Email(String email, String domain) {
             this.email = email + "@" + domain;
+            setTitle();
+        }
+
+        public void setTitle() {
+            this.title = "Email";
         }
 
         public String getEmail() {
@@ -148,7 +209,7 @@ public class Contact implements ContactInfo {
 
         @Override
         public String getTitle() {
-            return "Email";
+            return title;
         }
 
         @Override
@@ -156,5 +217,49 @@ public class Contact implements ContactInfo {
             return this.getEmail();
         }
     }
+
+    public class Tel implements ContactInfo {
+
+        private String tel;
+
+        private String title;
+
+        public Tel(int code, String number) {
+            this.tel = "+" + code + " " + number;
+        }
+
+        public void setTitle() {
+            this.title = "Tel";
+        }
+
+        public String getTel() {
+            return tel;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public String getValue() {
+            return this.getTel();
+        }
+    }
+
+    private class NameContactInfo implements ContactInfo {
+
+
+        @Override
+        public String getTitle() {
+            return null;
+        }
+
+        @Override
+        public String getValue() {
+            return null;
+        }
+    }
+
 
 }
